d136034d8063563e8a26e55532a7802b8c625d04
EC2 Default User <ec2-user@ip-172-31-16-214.us-east-2.compute.internal>
2021-06-11 01:00:53 +0000

    finished cloudformation

--- dynamo_api/deployment.sh
+++ dynamo_api/deployment.sh
@@ -33,5 +33,5 @@ fi
 
 if [[ $DEPLOY == 1 ]]
 then
-    aws cloudformation deploy --template-file packaged-template.json --stack-name application-stack2 --capabilities CAPABILITY_IAM
+    aws cloudformation deploy --template-file packaged-template.json --stack-name application-stack2  --capabilities CAPABILITY_NAMED_IAM
 fi
--- dynamo_api/package/user.py
+++ dynamo_api/package/user.py
@@ -1,3 +1,4 @@
+
 import json
 import boto3
 import os
@@ -10,9 +11,14 @@ table = dynamodb.Table(users_table)
 def getUser(event, context):
     print(json.dumps({"running": True}))
     print(json.dumps(event))
+    
+    path = event["path"] # "/user/123"
+    array_path = path.split("/") # ["", "user", "123"]
+    user_id = array_path[-1]
+    
     response = table.get_item(
         Key={
-            'pk': 'user_',
+            'pk': user_id,
             'sk': 'age'
         }
     )
@@ -20,16 +26,28 @@ def getUser(event, context):
     print(item)
     return {
         'statusCode': 200,
-        'body': json.dumps('Hello from Lambda!')
+        'body': json.dumps(item)
     }
     
 def putUser(event, context):
     print(json.dumps({"running": True}))
     print(json.dumps(event))
+    
+    path = event["path"] # "/user/123"
+    array_path = path.split("/") # ["", "user", "123"]
+    user_id = array_path[-1]
+    
+    body = event["body"] #"{\n\t\"name\": \"Jack\",\n\t\"last_name\": \"Click\",\n\t\"age\": 21\n}"
+    body_object = json.loads(body)
+    
+    
     table.put_item(
         Item={
-            'pk': 'user_',
-            'sk': 'age'
+            'pk': user_id,
+            'sk': 'age',
+            'name': body_object['name'],
+            'last_name': body_object['last_name'],
+            'age': body_object['age']
         }
     )
     
